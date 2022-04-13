73f412ed76a4332a473f2ff25011baf265b3805c
adover134 <45279353+adover134@users.noreply.github.com>
2022-04-13 17:31:57 +0900

    외부키로 값 받기 테스트 추가

--- djongoTest/testapp/models.py
+++ djongoTest/testapp/models.py
@@ -4,7 +4,10 @@ from djongo import models
 class User(models.Model):
     u_id = models.TextField(primary_key=True, default="")
     u_name = models.TextField(default="홍길동")
-    u_email = models.EmailField(default="aaaaa@aaa.com")
+    u_email = models.EmailField(unique=True, default="aaaaa@aaa.com")
+    u_active_token = models.TextField(default="")
+    u_warn_count=models.IntegerField(default=0)
+    u_active = models.IntegerField(null=False, default=0)
     inactivated_date = models.DateField(null=True)
 
     class Meta:
@@ -17,6 +20,6 @@ class User(models.Model):
 class Manager(models.Model):
     u_id = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, db_column="u_id")
     m_tel = models.TextField(default="010-0101-1010")
-
+    u_name = models.TextField()
     class Meta:
         db_table = "manager"
--- djongoTest/testapp/views.py
+++ djongoTest/testapp/views.py
@@ -9,19 +9,12 @@ class UserViewSets(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-    @request_converting_to_object_id
-    def retrieve(self, request, *args, **kwargs):
-        return super().retrieve(self, request, args, kwargs)
-
-    @request_converting_to_object_id
     def partial_update(self, request, *args, **kwargs):
         return super().partial_update(self, request, args, kwargs)
 
-    @request_converting_to_object_id
     def update(self, request, *args, **kwargs):
         return super().update(self, request, args, kwargs)
 
-    @request_converting_to_object_id
     def destroy(self, request, *args, **kwargs):
         return super().destroy(self, request, args, kwargs)
 
@@ -30,18 +23,14 @@ class ManagerViewSets(ModelViewSet):
     queryset = Manager.objects.all()
     serializer_class = ManagerSerializer
 
-    @request_converting_to_object_id
     def retrieve(self, request, *args, **kwargs):
         return super().retrieve(self, request, args, kwargs)
 
-    @request_converting_to_object_id
     def partial_update(self, request, *args, **kwargs):
         return super().partial_update(self, request, args, kwargs)
 
-    @request_converting_to_object_id
     def update(self, request, *args, **kwargs):
         return super().update(self, request, args, kwargs)
 
-    @request_converting_to_object_id
     def destroy(self, request, *args, **kwargs):
         return super().destroy(self, request, args, kwargs)
