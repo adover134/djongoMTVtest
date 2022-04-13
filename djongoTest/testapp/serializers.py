eaebb547430fa23aa2569bf935d5be468a27368e
adover134 <45279353+adover134@users.noreply.github.com>
2022-04-13 17:00:25 +0900

    OneToOne 사용

--- djongoTest/testapp/serializers.py
+++ djongoTest/testapp/serializers.py
@@ -6,8 +6,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         # fields = '__all__'
-        # fields 변수에는 CRUD에 사용될 필드 값들을 적는다.
-        fields = ["u_name", "u_email", "inactivated_date", "u_access_token"]
+        fields = ["u_id", "u_name", "u_email", "inactivated_date"]
 
 
 class ManagerSerializer(serializers.ModelSerializer):
